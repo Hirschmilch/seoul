0caf1746443f87a1f52a81edb70f5a30fe5fc97b
seoul1 <herrtade@yahoo.de>
2014-08-18 14:52:07 +0200

    Update MMA Fiora by seoul.lua

--- MMA Fiora by seoul.lua
+++ MMA Fiora by seoul.lua
@@ -1,7 +1,6 @@
 if myHero.charName ~= "Fiora" then return end
 
 require 'VPrediction'
-require 'SOW'
 
 local qReady, eReady  = false, false
 local QRANGE, RRANGE = 600, 400
@@ -9,28 +8,28 @@ local levelChart = {SPELL_1, SPELL_2, SPELL_3}
 local VP = nil
 
 function CDHandler()
-    qReady = (myHero:CanUseSpell(_Q) == READY) 
-    eReady = (myHero:CanUseSpell(_E) == READY)
+	qReady = (myHero:CanUseSpell(_Q) == READY) 
+    	eReady = (myHero:CanUseSpell(_E) == READY)
 end
   
 function GodMode() 
-    if Menu.MainCombo.QSettings.UseQ and ts.target ~= nil and ValidTarget(ts.target, 600) then
-        CastQ() 
-    end
+    	if Menu.MainCombo.QSettings.UseQ and ts.target ~= nil and ValidTarget(ts.target, 600) then
+		CastQ() 
+    	end
   
-    if Menu.MainCombo.ESettings.UseE and ts.target ~= nil and ValidTarget(ts.target, 250) then
-       CastE()
-    end
+    	if Menu.MainCombo.ESettings.UseE and ts.target ~= nil and ValidTarget(ts.target, 250) then
+       		CastE()
+    	end
 end 
 
 function CastQ()
-    if GetDistance(ts.target) > 250 and qReady then
-       CastSpell(_Q, ts.target)
-    end
+	if GetDistance(ts.target) > 250 and qReady then
+		CastSpell(_Q, ts.target)
+    	end
 end
 
 function CastE()
-    nextAA = _G.MMA_NextAttackAvailability
+    	nextAA = _G.MMA_NextAttackAvailability
 	if nextAA > 0.1 and nextAA < 0.2  and eReady then
 		CastSpell(_E)
 		_G.MMA_ResetAutoAttack()
@@ -39,34 +38,34 @@ function CastE()
 end
         
 function KSQ()
-    for i, enemy in ipairs(GetEnemyHeroes()) do
-        qDmg = getDmg("Q", enemy, myHero)
-        if QREADY and enemy ~= nil and ValidTarget(enemy, 600) and enemy.health < qDmg then
-            CastSpell(_Q, enemy)
-        end
-    end
+    	for i, enemy in ipairs(GetEnemyHeroes()) do
+        	qDmg = getDmg("Q", enemy, myHero)
+        	if QREADY and enemy ~= nil and ValidTarget(enemy, 600) and enemy.health < qDmg then
+        	 CastSpell(_Q, enemy)
+		end
+    	end
 end
 
 function OnDraw()
-    if Menu.Misc.ToDraw.DrawQ then
-        DrawCircle(myHero.x, myHero.y, myHero.z, QRANGE, 0x111111)
-    end
+    	if Menu.Misc.ToDraw.DrawQ then
+        	DrawCircle(myHero.x, myHero.y, myHero.z, QRANGE, 0x111111)
+	    end
   
-    if Menu.Misc.ToDraw.DrawR then
-        DrawCircle(myHero.x, myHero.y, myHero.z, RRANGE, 0x111111)
-    end
+    	if Menu.Misc.ToDraw.DrawR then
+		DrawCircle(myHero.x, myHero.y, myHero.z, RRANGE, 0x111111)
+    	end
 end
 
 function AutoLevel()
-    if player.level == 1 then
-        LevelSpell(levelChart[Menu.Misc.AutoLevel.SkillAt1])
-    elseif player.level == 2 then
-        LevelSpell(levelChart[Menu.Misc.AutoLevel.SkillAt2])
-    elseif player.level == 3 then
-        LevelSpell(levelChart[Menu.Misc.AutoLevel.SkillAt3])
-    elseif player.level == 6 or player.level == 11 or player.level == 16 then
-        LevelSpell(SPELL_4)
-    end
+    	if player.level == 1 then
+        	LevelSpell(levelChart[Menu.Misc.AutoLevel.SkillAt1])
+    	elseif player.level == 2 then
+        	LevelSpell(levelChart[Menu.Misc.AutoLevel.SkillAt2])
+    	elseif player.level == 3 then
+        	LevelSpell(levelChart[Menu.Misc.AutoLevel.SkillAt3])
+    	elseif player.level == 6 or player.level == 11 or player.level == 16 then
+        	LevelSpell(SPELL_4)
+    	end
 end
 
 function OnLoad()
@@ -88,16 +87,16 @@ function OnLoad()
 		
 	  	Menu.Misc:addSubMenu("Auto Level", "AutoLevel")
       			Menu.Misc.AutoLevel:addParam("SkillAt1", "Level 1", SCRIPT_PARAM_LIST, 1, { "_Q", "_W", "_E"})
-			 Menu.Misc.AutoLevel:addParam("SkillAt2", "Level 2", SCRIPT_PARAM_LIST, 1, { "_Q", "_W", "_E"})
-			 Menu.Misc.AutoLevel:addParam("SkillAt3", "Level 3", SCRIPT_PARAM_LIST, 1, { "_Q", "_W", "_E"})
+				Menu.Misc.AutoLevel:addParam("SkillAt2", "Level 2", SCRIPT_PARAM_LIST, 1, { "_Q", "_W", "_E"})
+			 	Menu.Misc.AutoLevel:addParam("SkillAt3", "Level 3", SCRIPT_PARAM_LIST, 1, { "_Q", "_W", "_E"})
       			Menu.Misc.AutoLevel:addParam("UseAutoLevel", "Use Auto Level", SCRIPT_PARAM_ONOFF, false)
          	
-    Menu:addSubMenu("Combo Settings", "MainCombo")
-    	Menu.MainCombo:addSubMenu("Q Settings", "QSettings")
-		Menu.MainCombo.QSettings:addParam("UseQ", "Use Q", SCRIPT_PARAM_ONOFF, true) 
-      	Menu.MainCombo:addSubMenu("E Settings", "ESettings")
-        	Menu.MainCombo.ESettings:addParam("UseE", "Use E", SCRIPT_PARAM_ONOFF, true)
-      	Menu.MainCombo:addParam("GodKey", "God Key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+	Menu:addSubMenu("Combo Settings", "MainCombo")
+    		Menu.MainCombo:addSubMenu("Q Settings", "QSettings")
+			Menu.MainCombo.QSettings:addParam("UseQ", "Use Q", SCRIPT_PARAM_ONOFF, true) 
+      		Menu.MainCombo:addSubMenu("E Settings", "ESettings")
+        		Menu.MainCombo.ESettings:addParam("UseE", "Use E", SCRIPT_PARAM_ONOFF, true)
+      		Menu.MainCombo:addParam("GodKey", "God Key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
 				
 end
 
